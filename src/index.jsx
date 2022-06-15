c0578f275de9c24701a198cf20af247dc2a1d09a
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-06-15 12:59:50 +0300

    refactor: remove strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -4,20 +4,12 @@ import { Provider } from 'react-redux';
 import { setupStore } from './store';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = setupStore();
 
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
