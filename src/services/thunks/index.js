399985fc956c2e84166cd7b9eae147bb98f84081
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-06-16 01:12:02 +0300

    feat: extent redux state with new slice

--- src/services/thunks/index.js
+++ src/services/thunks/index.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import getRates from '../requests';
 
 const fetchRates = createAsyncThunk(
-  'rates/fetchRates',
+  'ratesSlice/fetchRates',
   async ({ base, amount } = { base: 'USD', amount: 1 }) => {
     const response = await getRates(base, amount);
     return response.json();
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import converterSlice from './reducers/converterSlice';
 import ratesSlice from './reducers/ratesSlice';
+
+const rootReducer = combineReducers({
+  ratesSlice,
+  converterSlice
+});
 
 export const setupStore = () => {
   return configureStore({
-    reducer: { rates: ratesSlice }
+    reducer: rootReducer
   });
 };
