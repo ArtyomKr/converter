5dc5d76a8008d49a6842656638bf9fea4c068961
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-06-16 12:47:42 +0300

    feat: add styles to Convertor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import './App.scss';
-import fetchRates from './services/thunks';
 import Converter from './components/Сonverter';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchRates())
-      .unwrap()
-      .then((rates) => console.log(rates))
-      .catch((err) => console.log(err.message));
-  }, [dispatch]);
-
   return (
-    <div className="App">
+    <main className="App">
       <Converter />
-    </div>
+    </main>
   );
 }
 
